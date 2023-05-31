75df268df49663dc0837cbaf0a982b60e7ddf655
florent decoster <>
2023-05-31 22:24:19 +0200

    add mapper

---
+++ SeLoger.Application/Mapper/ClassifiedAdMapper.cs
@@ -0,0 +1,22 @@
+﻿using SeLoger.Application.Dtos;
+using SeLoger.Domain;
+
+namespace SeLoger.Application.Mapper;
+
+internal static class ClassifiedAdMapper
+{
+    public static ClassifiedAdDto ToDto(ClassifiedAd classifiedAd, double celsiusTemperature)
+    {
+        return new ClassifiedAdDto
+        {
+            Id = classifiedAd.Id.ToString("D"),
+            Title = classifiedAd.Title,
+            Description = classifiedAd.Description,
+            Latitude = classifiedAd.Latitude,
+            Longitude = classifiedAd.Longitude,
+            Status = (int) classifiedAd.Status,
+            Type = (int) classifiedAd.Type,
+            CelsiusTemperature = celsiusTemperature
+        };
+    }
+}
